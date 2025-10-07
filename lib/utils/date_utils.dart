932f558d1e40cd0db08149ab2a80a7d82b0dc345
Aum Shah <23cs085@charusat.edu.in>
2025-10-07 20:48:48 +0530

    Delete lib/utils directory

--- lib/utils/date_utils.dart
+++
@@ -1,3 +0,0 @@
-String nowIso() => DateTime.now().toUtc().toIso8601String();
-
-DateTime parseIso(String s) => DateTime.parse(s);
--- lib/utils/id_generator.dart
+++
@@ -1,5 +0,0 @@
-import 'package:uuid/uuid.dart';
-
-final _uuid = Uuid();
-
-String generateId() => _uuid.v4();
